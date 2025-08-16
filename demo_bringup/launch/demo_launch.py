095896b5ed182661048491fe7b1a993b5e1a8403
liyou <liyou117@foxmail.com>
2025-08-17 01:02:03 +0800

    添加参数文件和更新启动文件以配置主题发布者节点的参数

--- demo_bringup/launch/demo_launch.py
+++ demo_bringup/launch/demo_launch.py
@@ -1,7 +1,12 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    bringup_dir = get_package_share_directory('demo_bringup')
+    param_file = os.path.join(bringup_dir, 'param', 'demo_param.yaml')
+
     return LaunchDescription([
         Node(
             package='demo_topic',
@@ -9,9 +14,7 @@ def generate_launch_description():
             name='topic_publisher_node',
             output='screen',
             emulate_tty=True,
-            parameters=[
-                {"param_num": 17}
-            ]
+            parameters=[param_file]
         ),
         Node(
             package='demo_topic',
