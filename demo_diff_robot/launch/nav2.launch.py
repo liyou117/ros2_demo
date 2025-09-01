3f995664ae7844adeb6aa38540c5ab6317c8f735
liyou <liyou117@foxmail.com>
2025-09-02 00:56:58 +0800

    添加房间地图和导航参数配置，创建新的启动文件以支持模拟环境

---
+++ demo_diff_robot/launch/nav2.launch.py
@@ -0,0 +1,46 @@
+import os
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    # 获取与拼接默认路径
+    package_share_path = get_package_share_directory('demo_diff_robot')
+    nav2_bringup_path = get_package_share_directory('nav2_bringup')
+    rviz_config_path = os.path.join(nav2_bringup_path, 'rviz', 'nav2_default_view.rviz')
+
+    # 创建 Launch 配置
+    use_sim_time = launch.substitutions.LaunchConfiguration(
+        'use_sim_time', default='true')
+    map_yaml_path = launch.substitutions.LaunchConfiguration(
+        'map', default=os.path.join(package_share_path, 'config', 'map', 'room.yaml'))
+    nav2_param_path = launch.substitutions.LaunchConfiguration(
+        'params_file', default=os.path.join(package_share_path, 'config', 'nav2_params.yaml'))
+
+    return launch.LaunchDescription([
+        # 声明新的 Launch 参数
+        launch.actions.DeclareLaunchArgument('use_sim_time', default_value=use_sim_time,
+                                             description='Use simulation (Gazebo) clock if true'),
+        launch.actions.DeclareLaunchArgument('map', default_value=map_yaml_path,
+                                             description='Full path to map file to load'),
+        launch.actions.DeclareLaunchArgument('params_file', default_value=nav2_param_path,
+                                             description='Full path to param file to load'),
+
+        launch.actions.IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [nav2_bringup_path, '/launch', '/bringup_launch.py']),
+            # 使用 Launch 参数替换原有参数
+            launch_arguments={
+                'map': map_yaml_path,
+                'use_sim_time': use_sim_time,
+                'params_file': nav2_param_path}.items(),
+        ),
+        launch_ros.actions.Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_path],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+    ])
